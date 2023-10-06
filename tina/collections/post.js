14b8842387243bbb70f21413cad11e1e77f3ca54
George Bidilica <george.bidilica@outlook.com>
2023-10-06 11:21:03 +0300

    reproduction

--- tina/collections/post.js
+++ tina/collections/post.js
@@ -7,23 +7,79 @@ export default {
   path: "content/post",
   fields: [
     {
-      type: "string",
-      label: "Title",
-      name: "title",
-    },
-    {
-      type: "string",
-      label: "Blog Post Body",
-      name: "body",
-      isBody: true,
-      ui: {
-        component: "textarea",
-      },
-    },
+      type: "object",
+      label: "Blocks",
+      name: "blocks",
+      list: true,
+      templates: [
+        {
+          type: "object",
+          label: "Hero",
+          name: "hero",
+          fields: [
+            {
+              type: "string",
+              label: "Label",
+              name: "label"
+            },
+            {
+              type: "object",
+              label: "Appearance",
+              name: "appearance",
+              fields: [
+                {
+                  type: "string",
+                  label: "Free Text",
+                  name: "free_text",
+                  // this won't work
+                  ui: {
+                    defaultItem: {
+                      free_text: "placeholder"
+                    }
+                  }
+                },
+                {
+                  type: "string",
+                  label: "Size",
+                  name: "size",
+                  options: [
+                    "sm",
+                    "md"
+                  ]
+                },
+                {
+                  type: "string",
+                  label: "Color",
+                  name: "color",
+                  options: [
+                    "red",
+                    "blue"
+                  ],
+                  // this won't work
+                  ui: {
+                    defaultItem: {
+                      color: "blue"
+                    }
+                  }
+                }
+              ]
+            }
+          ],
+          // this will work
+          ui: {
+            defaultItem: {
+              appearance: {
+                size: "md",
+              }
+            }
+          }
+        }
+      ],
+    }
   ],
   ui: {
     router: ({ document }) => {
       return `/posts/${document._sys.filename}`;
     },
-  },
+  }
 };
--- tina/config.js
+++ tina/config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "tinacms";
-import page from "./collections/page";
 import post from "./collections/post";
 
 export const config = defineConfig({
@@ -26,7 +25,7 @@ export const config = defineConfig({
     outputFolder: "admin", // within the public folder
   },
   schema: {
-    collections: [page, post],
+    collections: [post],
   },
 });
 
